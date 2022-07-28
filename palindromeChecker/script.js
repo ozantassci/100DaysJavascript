e74dc11470a5911f52a5bf64fc6b7c45c1e9f767
Ozan TAŞCI <102819318+ozantassci@users.noreply.github.com>
2022-07-28 09:22:12 +0300

    Add files via upload

---
+++ palindromeChecker/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+const palindrome = function () {
+  const word = document.querySelector(".input-text").value;
+
+  let len = word.length;
+
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+
+  let flip = end.split("").reverse().join("");
+
+  if (start === flip) {
+    result.innerHTML = `✅ ${word.toUpperCase()} is a palindrome ✅`;
+  } else {
+    result.innerHTML = `❌ ${word.toUpperCase()} is NOT a palindrome ❌`;
+  }
+};
+
+btn.addEventListener("click", palindrome);
+
+
+
+
