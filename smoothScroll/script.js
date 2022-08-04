9888fe7d170d9957f513685a435b7fd8a51608b5
Ozan TAŞCI <102819318+ozantassci@users.noreply.github.com>
2022-08-04 10:08:45 +0300

    Add files via upload

---
+++ smoothScroll/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const links = document.querySelectorAll(".nav-list li a");
+
+for (let link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
