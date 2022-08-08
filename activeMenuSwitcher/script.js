8c4f942a3780b222a8b8b9c6e09cb596007b9f4e
Ozan TAŞCI <102819318+ozantassci@users.noreply.github.com>
2022-08-08 19:15:45 +0300

    Add files via upload

---
+++ activeMenuSwitcher/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const navList = document.querySelector(".nav-list");
+
+navList.addEventListener("click", (e) => {
+  const navLink = e.target.parentElement;
+
+  if (navLink.classList.contains("link")) {
+    navList.querySelector(".active").classList.remove("active");
+    navLink.classList.add("active");
+  }
+});
