456917ae70b64b05a141a00172f02d1dec69a263
Ozan TAŞCI <102819318+ozantassci@users.noreply.github.com>
2022-08-18 13:44:17 +0300

    Add files via upload

---
+++ pageScrollIndicator/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const links = document.querySelectorAll(".nav-list li a");
+
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+// Sticky Header
+window.addEventListener("scroll", () => {
+  const Header = document.querySelector("header");
+  Header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Scroll Indicator JS
+window.onscroll = () => scrollProgress;
+
+function scrollProgress() {
+  const currentState =
+    document.body.scrollTop || document.documentElement.scrollTop;
+
+  const pageHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const scrollPercentage = (currentState / pageHeight) * 100;
+
+  const progressBar = document.querySelector(".progress");
+
+  progressBar.style.visibility = "visible";
+  progressBar.style.width = scrollPercentage + "%";
+}
